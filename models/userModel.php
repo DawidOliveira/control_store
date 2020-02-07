b1bf0a29e10f80828eb5ef06f83fc563fa4238c0
Dawid Oliveira <dawid.oliveira@arapiraca.ufal.br>
2020-02-07 20:41:05 -0300

    função signUp funcionando

--- models/userModel.php
+++ models/userModel.php
@@ -15,10 +15,15 @@
             try{
 
                 $pass = md5($senha);
+                $hoje = date('y.m.d');
 
-                $query = $conn->query("INSERT into funcionarios (cpf,nome,senha,salario,numero_conta,cargo,contratado_em) values ('$cpf','$nome','$pass',$)");
+                $query = $this->conn->query("INSERT into funcionarios (cpf,nome,senha,salario,numero_conta,cargo,contratado_em) values ('$cpf','$nome','$pass','$salario','$numero_conta','$cargo','$hoje');");
+
+                echo "<script>alert('Cadastrado com sucesso!');</script>";
 
             }catch(Exception $e){
+
+                echo("<script>alert('Erro ao inserir dados no banco!\nErro: $e')</script>");
 
             }
 
--- views/teste.php
+++ views/teste.php
@@ -1,5 +1,7 @@
 <?php
+    include('../models/userModel.php');
+    $user = new UserModel();
 
-
+    $user->signUp("222.222.222-22","d","123","100","12.123-11","funcionario");
 
 ?>
