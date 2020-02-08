45dc15d8ad99b425e42817f05701b85822b7644b
Gean Lucas <gean.lfpsa@gmail.com>
2020-02-08 01:37:31 -0300

    Adição do cadastro de produtos

--- view/index.php
+++ view/index.php
@@ -9,14 +9,14 @@
   <!-- Material Design for Bootstrap fonts and icons -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons">
 
-  <link rel="shortcut icon" href="assets/user.png" type="image/x-icon">
+  <link rel="shortcut icon" href="../assets/user.png" type="image/x-icon">
 
   <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="../css/style.css">
   <!--<link rel="stylesheet" href="css/bootstrap.min.css">-->
-  <link rel="stylesheet" href="css/bootstrap-material-design.min.css">
+  <link rel="stylesheet" href="../css/bootstrap-material-design.min.css">
 
-  <title>Cadastrar funcionário</title>
+  <title>Página inicial</title>
 </head>
 
 <body>
@@ -38,7 +38,7 @@
         <div class="container">
           <div class="row">
             <div class="col-sm d-flex align-items-sm-center">
-              <img src="assets/account.png" class="img-fluid" alt="Usuário">
+              <img src="../assets/account.png" class="img-fluid" alt="Usuário">
             </div>
             <div class="col-lg">
               <a href="#" class="h5 font-weight-bold text-white">Usuário</a>
@@ -50,8 +50,11 @@
           </div>
         </div>
       </header>
-      <ul class="list-group">
+      <ul class="list-group pt-0">
         <li class="active list-group-item">
+          <a href="#" class="text-white">Home</a>
+        </li>
+        <li class=" list-group-item">
           Vendas
         </li>
         <li class="list-group-item">
@@ -64,90 +67,20 @@
     </div>
     <main class="bmd-layout-content">
       <div class="container">
-        <div class="card mt-3">
-          <div class="card-body">
-            <div class="card-title display-4">
-              Cadastrar funcionário
-            </div>
-            <div class="card-subtitle text-muted">Cadastre um novo funcionário no sistema</div>
-            <div class="card-body">
-              <form action="#" method="POST">
-                <div class="row">
-                  <div class="col">
-                    <div class="input-group mb-3">
-                      <div class="input-group-prepend mx-4 my-2 ">
-                        <img src="assets/user.png" alt="user" width="32px" height="32px">
-                      </div>
-                      <input type="text" class="form-control" placeholder="Nome" aria-label="Nome" value="" name="name" aria-describedby="basic-addon1">
-                    </div>
-                  </div>
-                  <div class="col">
-                    <div class="input-group mb-3">
-                      <div class="input-group-prepend mx-4 my-2 ">
-                        <img src="assets/user.png" alt="user" width="32px" height="32px"> </div>
-                      <input type="text" class="form-control" placeholder="CPF" aria-label="CPF" name="cpf" aria-describedby="basic-addon1">
-                    </div>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col">
-                    <div class="input-group mb-3">
-                      <div class="input-group-prepend mx-4 my-2 ">
-                        <img src="assets/lock.png" alt="user" width="32px" height="32px"> </div>
-                      <input type="text" class="form-control" placeholder="Senha" aria-label="Senha" name="password" aria-describedby="basic-addon1">
-                    </div>
-                  </div>
-                  <div class="col">
-                    <div class="input-group mb-3">
-                      <div class="input-group-prepend mx-4 my-2 ">
-                        <img src="assets/confirm.png" alt="user" width="32px" height="32px"> </div>
-                      <input type="text" class="form-control" placeholder="Confirmar senha" aria-label="Confirmar senha" name="confirm_password" aria-describedby="basic-addon1">
-                    </div>
-                  </div>
-                </div>
-                <div class="row">
-                  <div class="col">
-                    <div class="input-group mb-3">
-                      <div class="input-group-prepend mx-4 my-2 ">
-                        <img src="assets/price.png" alt="user" width="32px" height="32px"> </div>
-                      <input type="text" class="form-control" placeholder="Salário" aria-label="Salário" name="salary" aria-describedby="basic-addon1">
-                    </div>
-                  </div>
-                  <div class="col">
-                    <div class="input-group mb-3">
-                      <div class="input-group-prepend mx-4 my-2 ">
-                        <img src="assets/bank.png" alt="user" width="32px" height="32px"> </div>
-                      <input type="text" class="form-control" placeholder="Número da conta" aria-label="Número da conta" name="bank-account" aria-describedby="basic-addon1">
-                    </div>
-                  </div>
-                </div>
-                <div class="input-group mb-3">
-                  <div class="input-group-prepend mx-4 my-2 ">
-                    <img src="assets/wheel.png" alt="user" width="32px" height="32px"></div>
-                  <select class="form-control" name="cargo">
-                    <div class="my-4">
-                      <option class="form-control">Cargo do novo funcionário</option>
-                      <option class="form-control" value="func">Funcionário</option>
-                      <option class="form-control" value="manager">Gerente</option>
-                    </div>
-                  </select>
-                </div>
-                <button type="submit" class="btn btn-outline-primary btn-lg btn-block">Cadastrar</button>
-              </form>
-            </div>
-          </div>
-        </div>
+        paginas:
+        <a href="/control_store/view/cadFuncionario">Cadastrar funcionario</a>
+        <a href="/control_store/view/cadProduto">Cadastrar produto</a>
       </div>
     </main>
   </div>
 
 </body>
 
-<script src="js/jquery-3.4.1.min.js"></script>
-<script src="js/popper.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/bootstrap.bundle.min.js"></script>
-<script src="js/bootstrap-material-design.min.js"></script>
+<script src="../js/jquery-3.4.1.min.js"></script>
+<script src="../js/popper.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+<script src="../js/bootstrap.bundle.min.js"></script>
+<script src="../js/bootstrap-material-design.min.js"></script>
 
 <script>
   $(document).ready(function() {
