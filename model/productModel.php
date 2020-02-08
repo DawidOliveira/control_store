9f33df191d286f323bee0e5a01bbd2e994e97f78
Dawid <dawid.oliveira@arapiraca.ufal.br>
2020-02-07 23:52:34 -0300

    CRUD - produtos - feito

--- model/productModel.php
+++ model/productModel.php
@@ -18,17 +18,75 @@
 
                 return true;
 
+            }catch(Exception $e){
+
+                echo("<script>alert('Erro ao inserir dados no banco!\nErro: $e')</script>");
+
+            }
+
+            return true;
+
+        }
+
+        function dadosProduto($cod){
+            
+            try{
+
+                $query = $this->conn->query("SELECT * from produtos where cod=$cod;");
+                $dados = $query->fetch_array();
+                $array = array(
+                    "cod"=>(string)$dados["cod"],
+                    "nome"=>(string)$dados["nome"],
+                    "valor"=>(string)$dados["valor"],
+                    "quantidade"=>(string)$dados["quantidade"],
+                    "descricao"=>(string)$dados["descricao"],
+                );
+                
+                return $array;
+
+            }catch(Exception $e){
+
+                echo("<script>alert('Erro ao inserir dados no banco!\nErro: $e')</script>");
+
+            }
+
+            return null;
+
+        }
+
+        function editarProduto($cod,$nome,$valor,$quantidade,$descricao){
+
+            try{
+
+                $query = $this->conn->query("UPDATE produtos SET nome='$nome',valor='$valor',quantidade='$quantidade',descricao='$descricao' WHERE cod = '$cod';");
+
                 return true;
 
             }catch(Exception $e){
 
                 echo("<script>alert('Erro ao inserir dados no banco!\nErro: $e')</script>");
 
-                return false;
+            }
+
+            return false;
+
+        }
+
+        function deletarProduto($cod){
+
+            try{
+
+                $query = $this->conn->query("DELETE from produtos WHERE cod='$cod';");
+
+                return true;
+
+            }catch(Exception $e){
+
+                echo("<script>alert('Erro ao inserir dados no banco!\nErro: $e')</script>");
 
             }
 
-            return true;
+            return false;
 
         }
 
